dc5ff317dd5613d7c5d08ad401a251ad503f924f
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-16 11:32:29 +0100

    Add backend build
    Change-Id: If543645a620a5eb6755e82d4272016004dfbfa2a

---
+++ backend/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from blue_pair.version import VERSION
+
+
+setup(
+    name='blue-pair',
+    description='bluePair(Pair recording app)',
+    version=VERSION,
+    url='https://blue-pair/',
+    author='NSE(Neuroscientific Software Engineering)',
+    author_email='bbp-ou-nse@groupes.epfl.ch',
+
+    install_requires=[
+        'tornado',
+        'redis',
+        'hiredis',
+        'numpy',
+        'pandas',
+        'bglibpy',
+        'bluepy',
+        'neurom'
+    ],
+    tests_require=['pytest', 'pytest-cov'],
+    packages=find_packages(exclude=[]),
+    scripts=[],
+)
