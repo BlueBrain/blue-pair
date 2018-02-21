8ec4971538fe5ae3a876ab1583eb87fd6b78e363
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-21 15:19:34 +0100

    Init vue filters
    Change-Id: Id95e24e1ebd83b8fd202659cfed89a99fab1c68a

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,10 @@ import iView from 'iview';
 
 import App from './App.vue';
 import store from './store';
+import filters from './filters';
 
 Vue.use(iView);
+filters.init();
 
 Vue.config.productionTip = true;
 
