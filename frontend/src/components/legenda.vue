912c54a7c686833847db5f766116740a7f2e840a
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-21 15:15:23 +0100

    Add top-bar and legenda components
    Change-Id: I7862fef54eacc1bb68886b0050101e991da788f3

---
+++ frontend/src/components/legenda.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <color-palette/>
+</template>
+
+<script>
+  import ColorPalette from './side-panel/circuit-tab/color-palette';
+
+  export default {
+    name: 'legenda-bar',
+    components: {
+      'color-palette': ColorPalette,
+    },
+  };
+</script>
---
+++ frontend/src/components/top-bar.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div class="top-bar">
+    <div class="title">
+      Pair recording App
+    </div>
+    <div class="status">
+      {{ status.message }}
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import store from '@/store';
+
+  export default {
+    name: 'top-bar',
+    data() {
+      return {
+        status: {
+          message: 'Ready',
+        },
+      };
+    },
+    mounted() {
+      store.$on('updateStatus', status => this.status = status);
+    },
+  };
+</script>
+
+
+<style scoped lang="scss">
+  .top-bar {
+    position: relative;
+    height: 28px;
+    line-height: 28px;
+    background-color: #3e1b3e;
+    padding: 0 .5em;
+    color: #fffefc;
+
+    .status {
+      position: absolute;
+      height: 100%;
+      right: 0;
+      top: 0;
+      padding-right: 12px;
+    }
+  }
+</style>
