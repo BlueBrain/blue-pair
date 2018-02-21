0c84f6419709d3c0a47665d4e54a6129ac6c16cb
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-21 18:09:27 +0100

    Add neuron-size component
    Change-Id: I1868ca3ae6f59e70fbca9b6da9432e151a120218

---
+++ frontend/src/components/side-panel/circuit-tab/neuron-size.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <Card>
+    <i-form :label-width="120" label-position="left">
+      <FormItem label="Point neuron size">
+        <Slider
+          :min="5"
+          :max="30"
+          :step="1"
+          :value="size"
+          @input="setPointNeuronSize"
+          show-input
+        ></Slider>
+      </FormItem>
+    </i-form>
+  </Card>
+</template>
+
+
+<script>
+  import store from '@/store';
+
+  export default {
+    name: 'neuron-size',
+    data() {
+      return {size: store.state.circuit.pointNeuronSize};
+    },
+    methods: {
+      setPointNeuronSize(size) {
+        store.$dispatch('setPointNeuronSize', size);
+      }
+    },
+  };
+</script>
+
+
+<style lang="scss" scoped>
+  .ivu-card {
+    margin-bottom: 12px;
+  }
+
+  .ivu-form-item {
+    margin-bottom: 0;
+  }
+</style>
+
