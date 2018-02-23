3cb56fa627901ebbb5de043dab5d884127aae5ef
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-23 10:07:08 +0100

    Add components to the app.vue
    Change-Id: Ic285e7c2b9d78c79b84b5dcf99cf7d5ba37692fc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
 
 <template>
-  <div id="app"></div>
+  <div>
+    <top-bar/>
+    <viewport-component/>
+    <side-panel/>
+    <legenda-bar/>
+  </div>
 </template>
 
 
 <script>
+  import TopBar from './components/top-bar';
+  import Viewport from './components/viewport';
+  import SidePanel from './components/side-panel';
+  import Legenda from './components/legenda';
+
   export default {
     name: 'app',
+    components: {
+      'top-bar': TopBar,
+      'viewport-component': Viewport,
+      'side-panel': SidePanel,
+      'legenda-bar': Legenda,
+    },
   };
 </script>
