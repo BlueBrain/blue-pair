a28eb7566348c9c30c777bc8f0621b453a7681cf
Pavlo Getta <pavlo.getta@epfl.ch>
2018-02-28 14:09:22 +0100

    Add dygraph component
    Change-Id: Ib0529f42a71813c94f32c97d7a5ebb991a3c1d46

---
+++ frontend/src/components/shared/dygraph.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="container" ref="graph"></div>
+</template>
+
+
+<script>
+  import Dygraph from 'dygraphs';
+
+  export default {
+    name: 'dygraph',
+    props: ['config', 'data'],
+    mounted() {
+      this.graph = new Dygraph(this.$refs.graph, this.data, this.config);
+    },
+    watch: {
+      data() {
+        this.graph.updateOptions({file: this.data});
+      },
+    },
+    beforeDestroy() {
+      this.graph.destroy();
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  .container {
+    height: 100%;
+  }
+</style>
