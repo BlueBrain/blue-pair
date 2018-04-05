6f718089cc7f8ab71ace4f1855c9bb36df3b61f5
Pavlo Getta <pavlo.getta@epfl.ch>
2018-04-05 11:42:15 +0200

    Copy to OpenShift pod only used morphologies, format code
    Change-Id: I2d7444a2acf0d379ae5608c02214813829d71a1e

--- backend/copy-circuit-data.sh
+++ backend/copy-circuit-data.sh
@@ -23,6 +23,9 @@ export RSYNC_RSH='oc rsh'
 circuit_config="$1"
 target_pod="$2"
 
+[ -z "$circuit_config" ] && echo "first parameter circuit_config is not set" && exit 1;
+[ -z "$target_pod" ] && echo "second parameter target_pod is not set" && exit 1;
+
 circuit_path=$(       cat $circuit_config | grep CircuitPath     | grep -oEi '[\S+\ ](\S*)$' | xargs)
 nrn_path=$(           cat $circuit_config | grep nrnPath         | grep -oEi '[\S+\ ](\S*)$' | xargs)
 morphology_path=$(    cat $circuit_config | grep MorphologyPath  | grep -oEi '[\S+\ ](\S*)$' | xargs)
@@ -33,35 +36,26 @@ target_file=$(        cat $circuit_config | grep TargetFile      | grep -oEi '[\
 
 
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Circuit config:' $circuit_config
 echo '### Dest pod:' $target_pod
 
-echo
-echo '########################################################################################'
-echo '### Start sync'
 
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
+echo '### Start sync'
+
+
+echo
+echo '####################################################################################################'
 echo '### Create circuit folder'
 echo '### Path:' "$target_pod:$circuit_path"
 oc rsh "$target_pod" mkdir -p "$circuit_path"
 echo '### Done'
 
-echo
-echo '########################################################################################'
-echo '### Sync circuit config'
-echo '### Source:' "$circuit_config"
-echo '### Destination:' "$target_pod:$circuit_config"
-rsync \
-    -Lh \
-    --progress \
-    --ignore-existing \
-    "$circuit_config" "$target_pod:$circuit_config"
-echo '### Done'
 
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync CellLibraryFile'
 echo '### Source:' "$circuit_path/$cell_lib_file"
 echo '### Destination:' "$target_pod:$circuit_path/$cell_lib_file"
@@ -72,15 +66,17 @@ rsync \
     "$circuit_path/$cell_lib_file" "$target_pod:$circuit_path/$cell_lib_file"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Create folder for MEComboInfoFile'
 echo '### Path:' "$target_pod:$(dirname "$me_combo_info_file")"
 oc rsh "$target_pod" mkdir -p "$(dirname $me_combo_info_file)"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync MEComboInfoFile'
 echo '### Source:' "$me_combo_info_file"
 echo '### Destination:' "$target_pod:$me_combo_info_file"
@@ -91,15 +87,17 @@ rsync \
     "$me_combo_info_file" "$target_pod:$me_combo_info_file"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Create nrnPath folder'
 echo '### Path:' "$target_pod:$nrn_path"
 oc rsh $target_pod mkdir -p "$nrn_path"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync nrnPath/nrn.h5'
 echo '### Source:' "$nrn_path"/nrn.h5
 echo '### Destination:' "$target_pod:$nrn_path"/nrn.h5
@@ -110,8 +108,9 @@ rsync \
     "$nrn_path"/nrn.h5 "$target_pod:$nrn_path"/nrn.h5
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync nrnPath/start.target'
 echo '### Source:' "$nrn_path"/start.target
 echo '### Destination:' "$target_pod:$nrn_path"/start.target
@@ -122,8 +121,9 @@ rsync \
     "$nrn_path"/start.target "$target_pod:$nrn_path"/start.target
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync nrnPath/nrn_summary.h5'
 echo '### Source:' "$nrn_path"/nrn_summary.h5
 echo '### Destination:' "$target_pod:$nrn_path"/nrn_summary.h5
@@ -134,8 +134,9 @@ rsync \
     "$nrn_path"/nrn_summary.h5 "$target_pod:$nrn_path"/nrn_summary.h5
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync nrnPath/nrn_positions.h5'
 echo '### Source:' "$nrn_path"/nrn_positions.h5
 echo '### Destination:' "$target_pod:$nrn_path"/nrn_positions.h5
@@ -146,35 +147,55 @@ rsync \
     "$nrn_path"/nrn_positions.h5 "$target_pod:$nrn_path"/nrn_positions.h5
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Create MorphologyPath folder'
 echo '### Path:' "$target_pod:$morphology_path"
 oc rsh $target_pod mkdir -p "$morphology_path"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
+echo '### Create MorphologyPath/ascii folder'
+echo '### Path:' "$target_pod:$morphology_path"/ascii
+oc rsh $target_pod mkdir -p "$morphology_path"/ascii
+echo '### Done'
+
+
+echo
+echo '####################################################################################################'
 echo '### Sync MorphologyPath/ascii'
 echo '### Source:' "$morphology_path"/ascii
 echo '### Destination:' "$target_pod:$morphology_path"
-rsync \
-    -rLkhz \
-    --ignore-existing \
-    --blocking-io \
-    --progress \
-    "$morphology_path"/ascii "$target_pod:$morphology_path"
+
+if [ ! -f tmp/circuit-morphology-list.txt ]; then
+    echo File tmp/circuit-morphology-list.txt is not found
+    exit 1
+fi
+
+while read morphology
+do
+    rsync \
+        -vLh \
+        --progress \
+        --ignore-existing \
+        "$morphology_path"/ascii/"$morphology".asc "$target_pod:$morphology_path"/ascii/"$morphology".asc
+done < tmp/circuit-morphology-list.txt
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Create METypePath folder'
 echo '### Path:' "$target_pod:$me_type_path"
 oc rsh $target_pod mkdir -p "$me_type_path"
 echo '### Done'
 
+
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
 echo '### Sync METypePath'
 echo '### Source:' "$me_type_path"
 echo '### Destination:' "$target_pod:$me_type_path/.."
@@ -185,6 +206,21 @@ rsync \
     "$me_type_path" "$target_pod:$me_type_path/.."
 echo '### Done'
 
+
+# Copy circuit config last as backend starts when this file is present
 echo
-echo '########################################################################################'
+echo '####################################################################################################'
+echo '### Sync circuit config'
+echo '### Source:' "$circuit_config"
+echo '### Destination:' "$target_pod:$circuit_config"
+rsync \
+    -Lh \
+    --progress \
+    --ignore-existing \
+    "$circuit_config" "$target_pod:$circuit_config"
+echo '### Done'
+
+
+echo
+echo '####################################################################################################'
 echo '### Sync finished'
