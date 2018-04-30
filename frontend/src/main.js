784788daf8f38739a47899e521cf333332f6c555
Pavlo Getta <pavlo.getta@epfl.ch>
2018-04-30 10:36:53 +0200

    Remove store component from Vue init as not needed
    Change-Id: I15725908fc19783ebb2f7ab6eace2e3acabd2185

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,6 @@ import Vue from 'vue';
 import iView from 'iview';
 
 import App from './App.vue';
-import store from './store';
 import filters from './filters';
 
 Vue.use(iView);
@@ -14,6 +13,5 @@ filters.init();
 Vue.config.productionTip = true;
 
 new Vue({
-  store,
   render: h => h(App),
 }).$mount('#app');
