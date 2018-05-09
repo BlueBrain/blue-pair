9e9f52842d8eb7a13715bc8c12406409891170d5
Pavlo Getta <pavlo.getta@epfl.ch>
2018-05-09 10:00:53 +0200

    Remove hovered-neuron-info component
    Change-Id: Ic07dee17ba89509dc71ac43d0b0488d8ca77b687

--- frontend/src/components/side-panel/circuit-tab.vue
+++ frontend/src/components/side-panel/circuit-tab.vue
@@ -3,7 +3,6 @@
   <div>
     <neuron-selector></neuron-selector>
     <neuron-prop-filter/>
-    <hover-neuron-info/>
   </div>
 </template>
 
@@ -11,14 +10,12 @@
 <script>
   import NeuronPropFilter from './circuit-tab/neuron-prop-filter.vue';
   import NeuronSelector from './circuit-tab/neuron-selector.vue';
-  import HoverNeuronInfo from './circuit-tab/hover-neuron-info.vue';
 
   export default {
     name: 'circuit-tab',
     components: {
       'neuron-prop-filter': NeuronPropFilter,
       'neuron-selector': NeuronSelector,
-      'hover-neuron-info': HoverNeuronInfo,
     },
   };
 </script>
