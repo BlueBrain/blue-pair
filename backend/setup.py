7d75ffd2de08f13b17ed39beca37f5a805df8a82
Pavlo Getta <pavlo.getta@epfl.ch>
2020-08-27 13:58:01 +0200

    Backend: fix dependency versions
    Change-Id: Id146f3624a48e742e51731bc8f7eff968d162bbb

--- backend/setup.py
+++ backend/setup.py
@@ -13,16 +13,17 @@ setup(
     author_email='bbp-ou-nse@groupes.epfl.ch',
 
     install_requires=[
-        'futures',
-        'tornado',
-        'redis',
-        'hiredis',
-        'numpy',
-        'pandas',
-        'bglibpy',
-        'bluepy',
-        'neurom',
-        'debugpy'
+        'futures>=3.1.1',
+        'tornado>=6.0.4',
+        'redis>=3.5.3',
+        'hiredis>=1.1.0',
+        'numpy>=1.19.1',
+        'pandas>=1.1.1',
+        'bglibpy>=4.3.19',
+        'bluepy>=0.14.14',
+        'bluepysnap>=0.6.1',
+        'neurom>=1.5.2',
+        'debugpy>=1.0.0rc2'
     ],
     tests_require=['pytest', 'pytest-cov'],
     packages=find_packages(exclude=[]),
